7c57558276cc5793d8454cf2c64ab88b38077a02
h3-saki <2564149591@qq.com>
2026-07-17 10:04:54 +0800

    test: 尝试更改判决标准为相关锋

--- debug_value/iqtest.py
+++ debug_value/iqtest.py
@@ -89,7 +89,7 @@ n = 4096
 Ts = 1 / fs
 carrier_freq = 200e3
 timeseq = np.arange(n) * Ts
-sampled_seq = readhex.readhex("adc_buffer_16384_iir.hex")
+sampled_seq = readhex.readhex("adc_buffer_16384_3.hex")
 sampled_seq_mean = np.mean(sampled_seq)
 sampled_seq_blocked = bpf_100k_300k(sampled_seq)
 i_seq = sampled_seq_blocked * np.cos(2 * np.pi * carrier_freq * timeseq)
@@ -101,7 +101,70 @@ q_seq = lpf_100k(q_seq)
 
 envelope_seq = (i_seq * i_seq) + (q_seq * q_seq)
 envelope_seq = np.sqrt(envelope_seq)
+envelope_seq_mean = np.mean(envelope_seq)
+envelope_seq -= envelope_seq_mean  # 均值归一化
+envelope_seq /= envelope_seq_mean
+freq_seq = []
+for i in range(1, len(sampled_seq)):
+    cross = (i_seq[i - 1] * q_seq[i]) - (i_seq[i] * q_seq[i - 1])
+    dot = (i_seq[i - 1] * i_seq[i]) + (q_seq[i - 1] * q_seq[i]) + 1e-6
+    delta_phi = cross / dot
+    f = delta_phi / (2 * np.pi * Ts)
+    freq_seq.append(f)
+freq_seq = np.array(freq_seq)
+freq_seq_mean = np.mean(freq_seq)
+freq_seq -= freq_seq_mean
 # envelope_seq = sampled_seq_blocked
-envelope_seq = np.abs(np.fft.fftshift(np.fft.fft(envelope_seq)))
-plt.plot(np.arange(-1 * 512e3, 512e3, 250), envelope_seq)
+corrlation_list = []
+for i in range(1, 11):
+    freq_test = i * 1e3
+    test_sig = np.cos(2 * np.pi * freq_test * timeseq)
+    env_corr = np.correlate(envelope_seq, test_sig)
+    corr_peak = np.max(env_corr)
+    corrlation_list.append(corr_peak)
+
+corrlation_list = np.array(corrlation_list)
+max_corr = np.max(corrlation_list)
+max_corr_index = np.argmax(corrlation_list)
+max_corr_index += 1
+max_corr_index *= 1e3
+
+freq_corr_list = []
+for i in range(1, 11):
+    freq_test = i * 1e3
+    test_sig = np.cos(2 * np.pi * freq_test * timeseq)
+    freq_corr = np.correlate(freq_seq, test_sig)
+    corr_peak = np.max(freq_corr)
+    freq_corr_list.append(corr_peak)
+freq_corr_list = np.array(freq_corr_list)
+max_freq_corr = np.max(freq_corr_list)
+max_freq_corr_index = np.argmax(freq_corr_list)
+max_freq_corr_index += 1
+max_freq_corr_index *= 1e3
+
+test_sig_cos = np.cos(2 * np.pi * freq_test * timeseq)
+test_sig_sin = np.sin(2 * np.pi * freq_test * timeseq)
+power_sin = test_sig_sin * envelope_seq
+power_cos = test_sig_cos * envelope_seq
+power_sin = np.sum(power_sin)
+power_cos = np.sum(power_cos)
+power = np.sqrt((power_sin * power_sin) + (power_cos * power_cos))
+power *= 2 / n
+test_sig_cos = np.cos(2 * np.pi * freq_test * timeseq)
+test_sig_sin = np.sin(2 * np.pi * freq_test * timeseq)
+freq_power_sin = test_sig_sin[:4095] * freq_seq
+freq_power_cos = test_sig_cos[:4095] * freq_seq
+freq_power_sin = np.sum(freq_power_sin)
+freq_power_cos = np.sum(freq_power_cos)
+freq_power = np.sqrt(
+    (freq_power_sin * freq_power_sin) + (freq_power_cos * freq_power_cos)
+)
+freq_power *= 2 / n
+print(max_corr_index)
+print(power)
+print(max_freq_corr_index)
+print(freq_power)
+plt.plot(
+    np.arange(-1 * 512e3, 512e3, 250), np.abs(np.fft.fftshift(np.fft.fft(envelope_seq)))
+)
 plt.show()
